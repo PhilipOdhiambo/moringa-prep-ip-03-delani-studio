aef256a4194c9c1e450bea543ed6d526a23f53ad
Philip Odhiambo Owino <jofonowino@gmail.com>
2021-06-18 23:33:24 +0300

    add hover effect on the portfolio to show the project name

--- js/script.js
+++ js/script.js
@@ -8,4 +8,15 @@ function toggleDetail() {
 
 $("document").ready(function() {
     $("#what-we-do .col-md-4").click(toggleDetail);
+
+    // Target mouseenter and mouseleave on portfolio
+    $(".portfolio .col-md-3").mouseenter(function () {
+        let projectStr = $('img',this).attr('src');
+        let projectName = projectStr.split('/').pop().split('.').shift();
+        $(this).append('<div class="layer">'+ projectName + '</div>');
+    });
+
+    $(".portfolio .col-md-3").mouseleave(function () {
+      $(".layer",this).remove();
+    });
 })
