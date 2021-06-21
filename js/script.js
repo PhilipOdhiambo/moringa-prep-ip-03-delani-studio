01f1fa1760f9b5ca0705b0a2661457523e368688
Philip Odhiambo Owino <jofonowino@gmail.com>
2021-06-21 10:29:46 +0300

    add custom alert box when user enter feedback

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-// Function to toggle what we do icons with
-// their details
-function toggleDetail() {
-    $("img", this).toggle();
-    $(".detail", this).toggle();
-}
-
 
 $("document").ready(function() {
-    $("#what-we-do .col-md-4").click(toggleDetail);
 
-    // Target mouseenter and mouseleave on portfolio
+    // Toggle what-we-do icon with the detail
+    $("#what-we-do .col-md-4").click(function() {
+        $("img", this).toggle();
+        $(".detail", this).toggle();
+        console;
+    });
+
+    // Target mouseenter and mouseleave on portfolio items
     $(".portfolio .col-md-3").mouseenter(function () {
         let projectStr = $('img',this).attr('src');
         let projectName = projectStr.split('/').pop().split('.').shift();
@@ -19,4 +18,30 @@ $("document").ready(function() {
     $(".portfolio .col-md-3").mouseleave(function () {
       $(".layer",this).remove();
     });
+
+    // Collect form data
+    $("form").submit(function(e) {
+        e.preventDefault();
+        let data = {}
+        $("form .form-control").each(function() {
+            data[this.name] = $(this).val();
+        })
+
+        if (data['name'] === '' || data['email'] === '') {
+            alert("Please fill in the details and submit");
+            return
+        }
+
+        // alert(`Thank you ${data.name} for reaching out. We appreciate your feedback.`);
+        $(".contact-us .alert").append(`
+            <h5 class="submit-alert">Thank you ${data.name} for contacting us. We appreciate your feedback.<br/> 
+            <button class="btn btn-outline-light mt-2 px-5">OK</button> 
+            </h5>
+        `).ready(function() {
+            $(".contact-us button").click(function() {
+                $(".submit-alert").remove();
+            })
+        })
+
+    })
 })
