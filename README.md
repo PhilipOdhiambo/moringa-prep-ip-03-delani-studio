35a8aceec94522ab39673a453f92c3d45febb4de
Philip Odhiambo Owino <jofonowino@gmail.com>
2021-06-21 12:54:52 +0300

    improve the submit function and added readme

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# IP3
+# DELANI STUDIO
+
+### Author:
+#### Philip Odhiambo
+
+
+### DESCRIPTION
+This website is for Delani Studios to showcase what we do and to be able to contact us.
+
+### Languages used
+
+- Javascript
+- HTML
+- CSS
+
+### Frameworks used
+
+- jQuery for javascript
+- Bootstrap for CSS
+
+### DEMO:
+Checkout the website [here]()
+
+### HOW TO INSTALL
+
+To get install on your machine follow these steps:
+1. Navigate to the directory you want to install the project
+1. On the command promp or termina run the commad
+
+    ```
+    git clone https://github.com/PhilipOdhiambo/moringa-prep-ip-03-delani-studio.git
+    ```
+1. Enter into the cloned project using command `` cd moringa-prep-ip-03-delani-studio``
+
+## Known bugs
+
+There are no kown bugs. Please contact me below if you find any.
+
+## CONTACT AND SUPPORT
+In case of bugs, collaboration or support, please contact me through email: *jofonowino@gmail.com*
+
+### Licence
+
+This work is protected under [MIT LICENCE](LICENSE).
+
+>>COPYRIGHT &copy; PHILIP OWINO 2021
--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 
 $("document").ready(function() {
 
-    // Toggle what-we-do icon with the detail
+    // Toggle what-we-do icon with the details
     $("#what-we-do .col-md-4").click(function() {
         $("img", this).toggle();
         $(".detail", this).toggle();
-        console;
     });
 
     // Target mouseenter and mouseleave on portfolio items
@@ -14,34 +13,39 @@ $("document").ready(function() {
         let projectName = projectStr.split('/').pop().split('.').shift();
         $(this).append('<div class="layer">'+ projectName + '</div>');
     });
-
     $(".portfolio .col-md-3").mouseleave(function () {
       $(".layer",this).remove();
     });
 
-    // Collect form data
+    // When submit button is clicked    
     $("form").submit(function(e) {
         e.preventDefault();
-        let data = {}
-        $("form .form-control").each(function() {
-            data[this.name] = $(this).val();
-        })
+        // Collect form data
+        let name = $("#name").val(), email = $("#email").val(), message = $("#message").val();
+        if (name ==='' || email === '' || message === '') {
+          alert("Kindly fill in all fields and submit");
+        } else {
+          showDialog(name);
 
-        if (data['name'] === '' || data['email'] === '') {
-            alert("Please fill in the details and submit");
-            return
         }
-
-        // alert(`Thank you ${data.name} for reaching out. We appreciate your feedback.`);
-        $(".contact-us .alert").append(`
-            <h5 class="submit-alert">Thank you ${data.name} for contacting us. We appreciate your feedback.<br/> 
-            <button class="btn btn-outline-light mt-2 px-5">OK</button> 
-            </h5>
-        `).ready(function() {
-            $(".contact-us button").click(function() {
-                $(".submit-alert").remove();
-            })
-        })
-
+       
     })
 })
+
+// Function to append and remove custom dialogue box
+function showDialog(name) {
+  $(".contact-us .alert").append(`
+    <h5 class="submit-alert">Thank you ${name} for contacting us. We appreciate your feedback.<br/> 
+    <button class="btn btn-outline-light mt-2 px-5">OK</button> 
+    </h5>`
+  )
+    .ready(function () {
+      // Clear the form fields
+      $(".contact-us button").click(function () {
+        
+        $(".submit-alert").remove();
+        location.reload();
+
+      });
+    });
+}
